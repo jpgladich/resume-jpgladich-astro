a24116db21a758ff9fb1fa221f7c0f6032adc346
jpgladich <jpgladich@gmail.com>
2025-11-26 20:20:13 -0300

    Deploy latest build

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://jpgladich.github.io',
-    base: 'resume-jpgladich-astro', 
+  site: 'https://jpgladich.github.io',
+  base: '/resume-jpgladich-astro/',
 });
